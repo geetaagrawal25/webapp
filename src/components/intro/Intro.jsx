11f976d39f6ff507f7d0f2c3b427c17c72fdf091
Geeta Agrawal <mgeetanjali25@gmail.com>
2022-07-15 23:27:21 +0530

    added content

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -25,7 +25,10 @@ const Intro = () => {
               <small>20+ Completed Projects</small>
             </article>
           </div> */}
-          <p>I’ve spent the last several months in a remote environment, working with HTML, CSS, JavaScript, building everything from landing pages to APIs. My experience as a teacher in an international school helped me collaborate with fellow developers from all over the world. Additionally, working with kids isn’t an easy task, however, it has helped me become efficient, productive, and fast even in a very stressful environment, which is an important skill in the tech industry.</p>
+          <p>100 Feet Studio is one of the best and well-known studio in Hyderabad for its services. 
+            We committed ourselves to provide quality training for our fitness and dance enthusiasts in a fun, 
+            upbeat, welcoming environment which is encouraging for all.We aim to encourage our clients, students to reach their full potential in every aspect.  We welcome all and have classes available for ages 3 to 65! To ensure classes are suitable for those who attend, we offer a range of options across styles, age groups and abilities. Whether you are aiming to stay fit, or become a dance champion etc.
+Our services consist of Zumba , Dance Fitness, Bollywood, HipHop, Afrobeat, Kizomba, Bellydance etc. We are grateful to have some amazing collaboration with corporations like Microsoft, Deloitte, Infosys and many more. We are proud to give so many smiles and happy moments to our clients and we will continue to serve everyone with a degree of excellence.</p>
           <a href="#contact" className="btn btn-primary">Let's Talk</a>
         </div>
       </div>
